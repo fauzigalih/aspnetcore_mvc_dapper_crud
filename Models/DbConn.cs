1676a7f516ddca400c576e5cd996916db274ca1f
Fauzi Galih <fauzigalihas@gmail.com>
2020-05-23 14:42:53 +0700

    Create DbConn.cs

---
+++ Models/DbConn.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace aspnetcore_mvc_dapper_crud.Models
+{
+    public class DbConn
+    {
+        public DbConn(DbConnection connection)
+        {
+            this.Connection = connection;
+        }
+
+        internal DbConnection Connection { get; }
+
+    }
+}
